4e57720afaee9cca02a5baa11c9a7eff3c7cef19
harsh90904 <diyoraharsh6@gmail.com>
2024-12-27 09:37:52 +0530

    hr

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,7 +2,9 @@ export const navbar = () => {
     return `
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Job Portle</a>
+           <a class="navbar-brand " href="#">
+                <img src="../images/logos/logo.png" alt="" class="">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -12,6 +14,12 @@ export const navbar = () => {
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.html">Company reviews</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.html">Salary guide</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/pages/signup.html">Signup</a>
